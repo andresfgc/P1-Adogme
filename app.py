0b50733ca2f50cfeb43dfbc131bdd0351ee1cb8e
Andresfgc <afgarciacano@gmail.com>
2024-01-15 10:50:18 +0000

    Add and apply Flask in the html files

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, render_template, request, flash
+# if os.path.exists("env.py"):
+#     import env
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/dogs")
+def dogs():
+    return render_template("dogs.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
